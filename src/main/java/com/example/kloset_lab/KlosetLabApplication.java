f11141b5de67a96b84f37851b07d07376bc76ab2
dragonwaterr <yongsu626@naver.com>
2026-01-19 15:20:27 +0900

    chore: Spring Boot 프로젝트 의존성 추가 및 초기화

---
+++ src/main/java/com/example/kloset_lab/KlosetLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.kloset_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlosetLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlosetLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kloset_lab/KlosetLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kloset_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlosetLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
