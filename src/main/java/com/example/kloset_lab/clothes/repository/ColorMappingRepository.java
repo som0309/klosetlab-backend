478799b997ce638d211e79886aa612e3189461ea
epdlrnldudnj <kje0023@naver.com>
2026-01-20 21:26:16 +0900

    feat: Clothes 도메인 repository 작성

---
+++ src/main/java/com/example/kloset_lab/clothes/repository/ColorMappingRepository.java
@@ -0,0 +1,6 @@
+package com.example.kloset_lab.clothes.repository;
+
+import com.example.kloset_lab.clothes.entity.ColorMapping;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ColorMappingRepository extends JpaRepository<ColorMapping, Long> {}
