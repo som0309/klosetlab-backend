b3bb4e226433370b54812174c77f0ada71a37d57
epdlrnldudnj <kje0023@naver.com>
2026-02-02 21:43:34 +0900

    style: spotless

--- src/main/java/com/example/kloset_lab/global/config/S3Config.java
+++ src/main/java/com/example/kloset_lab/global/config/S3Config.java
@@ -1,6 +1,5 @@
 package com.example.kloset_lab.global.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
@@ -12,16 +11,16 @@ public class S3Config {
     /*
 
 
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
+        @Value("${cloud.aws.credentials.access-key}")
+        private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
+        @Value("${cloud.aws.credentials.secret-key}")
+        private String secretKey;
 
 
-    @Value("${cloud.aws.region.static}")
-    private String region;
-*/
+        @Value("${cloud.aws.region.static}")
+        private String region;
+    */
     @Bean
     public S3Client s3Client() {
         // AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
